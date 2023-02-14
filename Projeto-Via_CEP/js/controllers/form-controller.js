6004e8e7f7ae1f7c75b381dfb0148775d1571762
Luan Pinheiro <lluanps@gmail.com>
2023-02-14 01:04:35 -0300

    Tratamento de evento no campo numero

--- Projeto-Via_CEP/js/controllers/form-controller.js
+++ Projeto-Via_CEP/js/controllers/form-controller.js
@@ -31,8 +31,22 @@ export function init() {
     state.btnClear = document.forms.newAddress.btnClear;
 
     state.errorCep = document.querySelector('[data-error="cep"]');
-    state.errorNumber = document.querySelector('[data-error="street"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
 
-    console.log(state);
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+}
 
+function handleInputNumberChange(event) {
+    if(event.target.value == "") {
+        setFormError("number", "Campo requerido");
+    }
+    else {
+        setFormError("number", "");
+    }
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
+    
 }
