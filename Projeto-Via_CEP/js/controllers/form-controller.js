baab66462aebb5ee80ce0798554fcfae0ee2f3f5
Luan Pinheiro <lluanps@gmail.com>
2023-02-14 10:10:51 -0300

    Verificando se cep é vaido e preenchendo automaticamente

--- Projeto-Via_CEP/js/controllers/form-controller.js
+++ Projeto-Via_CEP/js/controllers/form-controller.js
@@ -1,5 +1,6 @@
 import Address from '../models/address.js'
-import * as requestService from '../services/request-service.js'
+import * as addressService from '../services/address-service.js'
+import RequestException from '../services/exceptions/request-exceptions.js';
 
 function State() {
 
@@ -37,14 +38,37 @@ export function init() {
     state.inputNumber.addEventListener('change', handleInputNumberChange);
     state.btnClear.addEventListener('click', handleBtnClearClick);
     state.btnSave.addEventListener('click', handleBtnSaveClick);
+    state.inputCep.addEventListener('change', handleInputCepChange);
 
+}
 
+async function handleInputCepChange(event) {
+    const cep = event.target.value;
+
+    try {
+        const address = await addressService.findByCep(cep);
+
+         state.inputCity.value = address.city;
+        state.inputStreet.value = address.street;
+        state.address = address;
+
+        setFormError("cep", "");
+        state.inputNumber.focus();
+
+        console.log(address);
+    }
+    catch (e) {
+        state.inputStreet.value = "";
+        state.inputCity.value = "";
+        setFormError("cep", "Informe um CEP válido");
+    }
+    
 }
 
 async function handleBtnSaveClick(event) {
     event.preventDefault();
-    const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
-    console.log(result);
+    //const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
+    console.log(event.target);
 }
 
 function handleInputNumberChange(event) {
